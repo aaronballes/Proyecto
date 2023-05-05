1727a9fa498affe096e5721e434e875ad808d4d4
aaronballes <118268459+aaronballes@users.noreply.github.com>
2023-05-05 14:04:41 +0200

    Subo entidadesProyecto

---
+++ EntidadesProyecto/src/entidadesproyecto/Products.java
@@ -0,0 +1,45 @@
+
+package entidadesproyecto;
+
+
+public class Products {
+   private int product_id;
+   private Category category;
+   private String product_name;
+   private double price;
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public Category getCategory_id() {
+        return category;
+    }
+
+    public void setCategory_id(Category category) {
+        this.category = category;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+   
+   
+   
+}
